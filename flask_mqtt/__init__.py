908fd193b8a366f17428ee5b11376009c64cb130
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2017-04-02 12:52:06 +0200

    add unsubscribe and unsubscribe_all

--- flask_mqtt/__init__.py
+++ flask_mqtt/__init__.py
@@ -1,6 +1,6 @@
 import time
 from threading import Thread
-from paho.mqtt.client import Client, MQTTMessage
+from paho.mqtt.client import Client, MQTT_ERR_SUCCESS
 
 
 __version__ = '0.0.1'
@@ -11,6 +11,7 @@ class Mqtt():
     def __init__(self, app=None):
         self.app = app
         self.client = Client()
+        self.topics = []
         self.mqtt_thread = Thread(target=self.loop_forever)
         self.mqtt_thread.daemon = True
 
@@ -22,8 +23,10 @@ class Mqtt():
         self.connect()
 
     def connect(self):
-        self.client.username_pw_set(self.app.config['MQTT_USERNAME'], self.app.config['MQTT_PASSWORD'])
-        res = self.client.connect(self.app.config['MQTT_BROKER_URL'], self.app.config['MQTT_BROKER_PORT'])
+        self.client.username_pw_set(self.app.config['MQTT_USERNAME'],
+                                    self.app.config['MQTT_PASSWORD'])
+        res = self.client.connect(self.app.config['MQTT_BROKER_URL'],
+                                  self.app.config['MQTT_BROKER_PORT'])
         if res == 0:
             self.mqtt_thread.start()
 
@@ -42,7 +45,40 @@ class Mqtt():
         return decorator
 
     def subscribe(self, topic, qos=0):
-        return self.client.subscribe(topic, qos)
+        """
+        :todo: add support for list of topics
+
+        """
+        # don't subscribe if already subscribed
+        if topic in self.topics:
+            return
+
+        # try to subscribe
+        result, mid = self.client.subscribe(topic, qos)
+
+        # if successful add to topics
+        if result == MQTT_ERR_SUCCESS:
+            self.topics.append(topic)
+
+        return result
+
+    def unsubscribe(self, topic):
+        # don't unsubscribe if not in topics
+        if topic not in self.topics:
+            return
+
+        result, mid = self.client.unsubscribe(topic)
+
+        # if successful remove from topics
+        if result == MQTT_ERR_SUCCESS:
+            self.topics.remove(topic)
+
+        return result
+
+    def unsubscribe_all(self):
+        topics = self.topics[:]
+        for topic in topics:
+            self.unsubscribe(topic)
 
     def publish(self, topic, payload=None, qos=0, retain=False):
         return self.client.publish(topic, payload, qos, retain)
@@ -52,4 +88,3 @@ class Mqtt():
             self.client.on_message = handler
             return handler
         return decorator
-
