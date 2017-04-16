93da001797f2b4a863dfddad1b1e627d5bf32ac9
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2017-04-16 14:21:04 +0200

    remove app property if init_app is used

--- flask_mqtt/__init__.py
+++ flask_mqtt/__init__.py
@@ -11,6 +11,7 @@ class Mqtt():
     def __init__(self, app=None):
         self.app = app
         self.client = Client()
+        self.refresh_time = 1.0
         self.topics = []
         self.mqtt_thread = Thread(target=self.loop_forever)
         self.mqtt_thread.daemon = True
@@ -19,20 +20,22 @@ class Mqtt():
             self.init_app(app)
 
     def init_app(self, app):
-        self.app = app
-        self.connect()
+        self.refresh_time = app.config.get('MQTT_REFRESH_TIME', 1.0)
 
-    def connect(self):
+        self.connect(
+            username=app.config.get('MQTT_USERNAME'),
+            password=app.config.get('MQTT_PASSWORD'),
+            broker_url=app.config.get('MQTT_BROKER_URL', 'localhost'),
+            broker_port=app.config.get('MQTT_BROKER_PORT', 1883)
+        )
+
+    def connect(self, username, password, broker_url, broker_port):
         if not self.mqtt_thread.is_alive():
 
-            if 'MQTT_USERNAME' in self.app.config:
-                self.client.username_pw_set(self.app.config['MQTT_USERNAME'],
-                                            self.app.config['MQTT_PASSWORD'])
+            if username is not None:
+                self.client.username_pw_set(username, password)
 
-            res = self.client.connect(
-                self.app.config.get('MQTT_BROKER_URL', 'localhost'),
-                self.app.config.get('MQTT_BROKER_PORT', 1883)
-            )
+            res = self.client.connect(broker_url, broker_port)
 
             if res == 0:
                 self.mqtt_thread.start()
@@ -42,7 +45,7 @@ class Mqtt():
 
     def loop_forever(self):
         while True:
-            time.sleep(self.app.config.get('MQTT_REFRESH_TIME', 1.0))
+            time.sleep(self.refresh_time)
             self.client.loop(timeout=1.0, max_packets=1)
 
     def on_topic(self, topic):
