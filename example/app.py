c2cc30ced27d1ac44c78f8f37e3eab5064b57a3d
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2017-05-26 12:57:48 +0200

    Example: Connect to HiveMQTT testserver

--- example/app.py
+++ example/app.py
@@ -1,3 +1,9 @@
+"""
+
+A small Test application to show how to use Flask-MQTT.
+
+"""
+
 import eventlet
 import json
 from flask import Flask, render_template
@@ -10,9 +16,19 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 app.config['SECRET'] = 'my secret key'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'
 app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+
+# Parameters for SSL enabled
+# app.config['MQTT_BROKER_PORT'] = 8883
+# app.config['MQTT_TLS_ENABLED'] = True
+# app.config['MQTT_TLS_INSECURE'] = True
+# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
+
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
@@ -44,5 +60,10 @@ def handle_mqtt_message(client, userdata, message):
     socketio.emit('mqtt_message', data=data)
 
 
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True)
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True, debug=True)
