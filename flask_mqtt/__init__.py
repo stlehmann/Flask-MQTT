a7f178e3911a1e98eaaf16d725c1b316624eaf6f
David Luhmer <david-dev@live.de>
2017-12-14 20:47:46 +0100

    add support for websockets / add support to set mqtt client-id

--- flask_mqtt/__init__.py
+++ flask_mqtt/__init__.py
@@ -8,7 +8,7 @@ from paho.mqtt.client import Client, MQTT_ERR_SUCCESS, MQTT_ERR_ACL_DENIED, \
     MQTT_LOG_WARNING
 
 
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 
 
 class Mqtt():
@@ -16,7 +16,7 @@ class Mqtt():
     def __init__(self, app=None):
         # type: (Flask) -> None
         self.app = app
-        self.client = Client()
+        self.client = Client(app.config.get('MQTT_CLIENT_ID')) if app.config.get('MQTT_CLIENT_ID') else Client()
         self.client.on_connect = self._handle_connect
         self.client.on_disconnect = self._handle_disconnect
         self.topics = []  # type: List[str]
@@ -30,6 +30,7 @@ class Mqtt():
         self.username = app.config.get('MQTT_USERNAME')
         self.password = app.config.get('MQTT_PASSWORD')
         self.broker_url = app.config.get('MQTT_BROKER_URL', 'localhost')
+        self.transport = app.config.get('MQTT_BROKER_TRANSPORT', 'tcp')
         self.broker_port = app.config.get('MQTT_BROKER_PORT', 1883)
         self.tls_enabled = app.config.get('MQTT_TLS_ENABLED', False)
         self.keepalive = app.config.get('MQTT_KEEPALIVE', 60)
