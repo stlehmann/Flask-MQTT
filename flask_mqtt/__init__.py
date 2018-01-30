743b35c609c63d8f71d566e3a05d17e50b5f3eed
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2018-01-30 22:55:40 +0100

    Version 0.0.11

--- flask_mqtt/__init__.py
+++ flask_mqtt/__init__.py
@@ -9,7 +9,7 @@ from paho.mqtt.client import Client, MQTT_ERR_SUCCESS, MQTT_ERR_ACL_DENIED, \
     MQTT_LOG_WARNING
 
 
-__version__ = '0.0.10'
+__version__ = '0.0.11'
 
 
 TopicQos = namedtuple('TopicQos', ['topic', 'qos'])
