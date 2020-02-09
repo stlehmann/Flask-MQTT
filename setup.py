ae6b5e7564a44036a088fcc888d0a534149b5dad
Stefan Lehmann <stlm@posteo.de>
2020-02-09 14:27:49 +0100

    setup.py: reformat with black

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names, **kwargs):
     """Open a file and read its content."""
     with io.open(
         os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8")
+        encoding=kwargs.get("encoding", "utf8"),
     ) as fp:
         return fp.read()
 
@@ -18,45 +18,41 @@ def read(*names, **kwargs):
 def find_version(*file_paths):
     """Find current package version number."""
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
 
-long_description = read('README.md')
+long_description = read("README.md")
 
 
-if sys.argv[-1] == 'test':
-    os.system('coverage run -m unittest discover -s tests')
-    os.system('coverage html --include flask_mqtt/*')
-    os.system('coverage report -m --include flask_mqtt/*')
+if sys.argv[-1] == "test":
+    os.system("coverage run -m unittest discover -s tests")
+    os.system("coverage html --include flask_mqtt/*")
+    os.system("coverage report -m --include flask_mqtt/*")
 else:
     setup(
-        name='Flask-MQTT',
-        version=find_version('flask_mqtt', '__init__.py'),
-        url='https://github.com/MrLeeh/Flask-MQTT',
-        license='MIT',
-        author='Stefan Lehmann',
-        author_email='stefan.st.lehmann@gmail.com',
-        description='Flask extension for the MQTT protocol',
+        name="Flask-MQTT",
+        version=find_version("flask_mqtt", "__init__.py"),
+        url="https://github.com/MrLeeh/Flask-MQTT",
+        license="MIT",
+        author="Stefan Lehmann",
+        author_email="stefan.st.lehmann@gmail.com",
+        description="Flask extension for the MQTT protocol",
         long_description=long_description,
-        long_description_content_type='text/markdown',
-        packages=['flask_mqtt'],
-        platforms='any',
+        long_description_content_type="text/markdown",
+        packages=["flask_mqtt"],
+        platforms="any",
         python_requires=">=3.6",
-        install_requires=[
-            'Flask',
-            'paho-mqtt'
-        ],
+        install_requires=["Flask", "paho-mqtt"],
         classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-            'Topic :: Software Development :: Libraries :: Python Modules'
-        ]
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Web Environment",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
     )
