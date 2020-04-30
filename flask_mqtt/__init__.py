608973474e126eca0603ec9396edd2a1448bf867
Stefan Lehmann <stlm@posteo.de>
2020-04-30 22:22:11 +0200

    Update version

--- flask_mqtt/__init__.py
+++ flask_mqtt/__init__.py
@@ -45,7 +45,7 @@ if sys.version_info[0] >= 3:
     unicode = str
 
 # current Flask-MQTT version
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 #: Container for topic + qos
 TopicQos = namedtuple("TopicQos", ["topic", "qos"])
