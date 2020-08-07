80e415685156bfc0e11e2128b2334ffcf0667d0b
andreas <al@exitzero.de>
2020-08-07 16:01:14 +0200

    tls example with test.mosquitto.org certs

---
+++ example/simple-mqtt-web-interface-tls/app.py
@@ -0,0 +1,88 @@
+"""
+
+A small Test application to show how to use Flask-MQTT.
+
+"""
+import logging
+
+import eventlet
+import json
+from flask import Flask, render_template
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
+from flask_mqtt import ssl
+
+
+eventlet.monkey_patch()
+
+app = Flask(__name__)
+app.config['SECRET'] = 'my secret key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
+app.config['MQTT_CLEAN_SESSION'] = True
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_LAST_WILL_TOPIC'] = 'home/lastwill'
+app.config['MQTT_LAST_WILL_MESSAGE'] = 'bye'
+app.config['MQTT_LAST_WILL_QOS'] = 2
+
+# Parameters for SSL enabled
+app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_BROKER_URL'] = 'test.mosquitto.org'
+app.config['MQTT_BROKER_PORT'] = 8884
+app.config['MQTT_TLS_INSECURE'] = False
+app.config['MQTT_TLS_CA_CERTS'] = 'mosquitto.org.crt'
+app.config['MQTT_TLS_CERTFILE'] = 'client.crt'
+app.config['MQTT_TLS_KEYFILE']  = 'client.key'
+app.config['MQTT_TLS_VERSION'] = ssl.PROTOCOL_TLSv1_2
+app.config['MQTT_TLS_CERT_REQS'] = True
+# if broker requires cert + user/pass then set this here
+#app.config['MQTT_USERNAME'] = ''
+#app.config['MQTT_PASSWORD'] = ''
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'], data['qos'])
+
+
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['topic'], data['qos'])
+
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode(),
+        qos=message.qos,
+    )
+    socketio.emit('mqtt_message', data=data)
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    # print(level, buf)
+    pass
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
